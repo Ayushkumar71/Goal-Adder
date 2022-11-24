9a8d668a7424e47b4b46f435e947f8f69a63c6bd
AyushKumar71 <ayush11092003@gmail.com>
2022-11-24 08:10:22 +0530

    Improved layout

--- App.js
+++ App.js
@@ -1,23 +1,62 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  // the useState hook looks like it updates "enteredGoalText", whenever "setEnteredGoalText" is called with an argument.
+  // Write this down in notes with a bracket saying that the words may be a little off.
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+
+  // Handler function for input text and button.
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+
+  }
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
+
   // JSX below
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>Hello World!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your goals.' onChangeText={goalInputHandler}/>
+        <Button title='Add Goal'onPress={addGoalHandler}/>
       </View>
-      <Text>This is me...</Text>
-      <Button title='Button'/>  
+      <View style={styles.goalsContainer}>
+        <Text>
+          List of Goals
+        </Text>
+      </View>
     </View>
   );
 }
 
+// Stylesheet objects written using properties similar to that of CSS.
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 35,
+  },
+  inputContainer: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 50,   
+    borderBottomColor: '#cccccc',
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 4,
+  },
+  goalsContainer: {
+    // Start from here
+    flex: 5,
   },
 }); 
